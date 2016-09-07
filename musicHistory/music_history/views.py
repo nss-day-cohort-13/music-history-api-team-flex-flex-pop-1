819f5a0852b52d00dc0ec829b6c469a0c60dca62
Michael Mead <michaelmead007@icloud.com>
2016-09-07 18:41:08 -0500

    set up views

--- musicHistory/music_history/views.py
+++ musicHistory/music_history/views.py
@@ -59,3 +59,85 @@ def artist_detail(request, pk, format=None):
     elif request.method == 'DELETE':
         artist.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def album_list(request, format=None):
+    """
+    List all code artist, or create a new snippet.
+    """
+    if request.method == 'GET':
+        album = Album.objects.all()
+        serializer = AlbumSerializer(album, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = AlbumSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def album_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a album instance.
+    """
+    try:
+        album = Album.objects.get(pk=pk)
+    except Album.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AlbumSerializer(album)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = AlbumSerializer(album, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        album.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def songs_list(request, format=None):
+    """
+    List all code songs, or create a new snippet.
+    """
+    if request.method == 'GET':
+        songs = Songs.objects.all()
+        serializer = SongsSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def songs_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a songs instance.
+    """
+    try:
+        songs = Songs.objects.get(pk=pk)
+    except Songs.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SongsSerializer(songs)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SongsSerializer(songs, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        songs.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
